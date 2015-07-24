b8ddbc72315ea5639b577e34cba0e1dde0471479
swaption2009 <swaption2009@gmail.com>
2015-07-24 16:02:55 -0700

    premium requirement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // clear the screen for testing
 document.body.innerHTML = '';
 
-var nums = [1,2,3];
+var nums = ["Apple", "Kitty", "Sunny", "Lily", "Timmy"];
 
 // Let's loop over the numbers in our array
 for (var i = 0; i < nums.length; i++) {
@@ -10,7 +10,8 @@ for (var i = 0; i < nums.length; i++) {
     var num = nums[i];
 
     // We're creating a DOM element for the number
-    var elem = document.createElement('div');
+    var elem = document.createElement('img');
+    elem.setAttribute('src', 'https://placekitten.com/g/200/300');
     elem.textContent = num;
 
     // ... and when we click, alert the value of `num`
